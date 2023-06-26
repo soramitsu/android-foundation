c04c1bd76f7872df7b5b0a1a06b1947222ee032f
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2023-06-26 15:31:03 +0400

    Remove precompiled scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,20 @@ allprojects {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+
+    dependencies {
+        classpath(libs.gradleplugins.android)
+        classpath(libs.gradleplugins.kotlin)
+    }
+}
--- runtime-permission/build.gradle.kts
+++ runtime-permission/build.gradle.kts
@@ -1,8 +1,22 @@
 plugins {
-    id("android-setup")
+    id("com.android.library")
+    kotlin("android")
 }
 
 android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+
+        kotlinOptions {
+            freeCompilerArgs += arrayOf(
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+            )
+        }
+    }
+
     namespace = "jp.co.soramitsu.foundation.runtime_permission"
 }
 
