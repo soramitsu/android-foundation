1d4e261ac81b9098dd8de3b20ebeb7db323e7ca6
arvifox <arvifox@hotmail.com>
2023-06-29 16:10:32 +0300

    share utils

---
+++ src/main/java/jp/co/soramitsu/androidfoundation/fragment/FragmentUtils.kt
@@ -0,0 +1,16 @@
+package jp.co.soramitsu.androidfoundation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+inline fun <T : androidx.fragment.app.Fragment> T.withArgs(argInitializer: (arguments: Bundle) -> Unit): T {
+  val args = arguments ?: Bundle()
+  argInitializer.invoke(args)
+  arguments = args
+  return this
+}
+
+inline fun <T : androidx.fragment.app.Fragment> T.applyArgs(argInitializer: Bundle.() -> Unit): T {
+  arguments = (arguments ?: Bundle()).apply(argInitializer)
+  return this
+}
---
+++ src/main/java/jp/co/soramitsu/androidfoundation/intent/IntentUtils.kt
@@ -0,0 +1,17 @@
+package jp.co.soramitsu.androidfoundation.intent
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import kotlin.system.exitProcess
+
+fun restartApplication(ctx: Context) {
+    val launchIntent = ctx.packageManager.getLaunchIntentForPackage(ctx.packageName)
+    val pendingStartIntent = PendingIntent
+        .getActivity(ctx, 0, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+    val mgr = ctx.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1000, pendingStartIntent)
+    exitProcess(2)
+}
+    
