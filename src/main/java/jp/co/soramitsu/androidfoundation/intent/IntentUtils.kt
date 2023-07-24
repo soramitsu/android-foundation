a00c22232072406884749615e4d2d8895a413d12
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2023-07-24 20:36:46 +0600

    Fixed android-foundation detekt issues

--- src/main/java/jp/co/soramitsu/androidfoundation/intent/IntentUtils.kt
+++ src/main/java/jp/co/soramitsu/androidfoundation/intent/IntentUtils.kt
@@ -4,6 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import kotlin.system.exitProcess
+import kotlin.time.Duration.Companion.seconds
 
 fun restartApplication(ctx: Context) {
     val launchIntent = ctx.packageManager.getLaunchIntentForPackage(ctx.packageName)
@@ -11,6 +12,6 @@ fun restartApplication(ctx: Context) {
         .getActivity(ctx, 0, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
     val mgr = ctx.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-    mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1000, pendingStartIntent)
+    mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1.seconds.inWholeMilliseconds, pendingStartIntent)
     exitProcess(2)
 }
--- src/main/java/jp/co/soramitsu/androidfoundation/phone/DeviceVibrator.kt
+++ src/main/java/jp/co/soramitsu/androidfoundation/phone/DeviceVibrator.kt
@@ -1,16 +1,15 @@
-package jp.co.soramitsu.androidfoundation
+package jp.co.soramitsu.androidfoundation.phone
 
 import android.os.Vibrator
 
 class DeviceVibrator(
     private val vibrator: Vibrator
 ) {
+    fun makeShortVibration() {
+        vibrator.vibrate(SHORT_VIBRATION_DURATION)
+    }
 
     companion object {
         private const val SHORT_VIBRATION_DURATION = 200L
-    }
-
-    fun makeShortVibration() {
-        vibrator.vibrate(SHORT_VIBRATION_DURATION)
     }
 }
