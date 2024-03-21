a55c1c17108a6ef0945f05d0bf0778a74f45adc3
arvifox <arvifox@hotmail.com>
2024-03-21 16:47:22 +0300

    mwr-575

--- src/main/java/jp/co/soramitsu/androidfoundation/phone/DeviceVibrator.kt
+++ src/main/java/jp/co/soramitsu/androidfoundation/phone/DeviceVibrator.kt
@@ -1,12 +1,17 @@
 package jp.co.soramitsu.androidfoundation.phone
 
+import android.os.Build
+import android.os.VibrationEffect
 import android.os.Vibrator
 
 class DeviceVibrator(
     private val vibrator: Vibrator
 ) {
     fun makeShortVibration() {
-        vibrator.vibrate(SHORT_VIBRATION_DURATION)
+        if (Build.VERSION.SDK_INT >= 26)
+            vibrator.vibrate(VibrationEffect.createOneShot(SHORT_VIBRATION_DURATION, VibrationEffect.DEFAULT_AMPLITUDE))
+        else
+            vibrator.vibrate(SHORT_VIBRATION_DURATION)
     }
 
     companion object {
