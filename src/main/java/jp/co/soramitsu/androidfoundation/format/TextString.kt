f1d1aa9a91512ba8aef3046fb458c90e40e3ec44
arvifox <arvifox@hotmail.com>
2024-04-21 15:58:42 +0300

    sc-29

--- src/main/java/jp/co/soramitsu/androidfoundation/format/TextString.kt
+++ src/main/java/jp/co/soramitsu/androidfoundation/format/TextString.kt
@@ -4,4 +4,6 @@ fun String.nullIfEmpty(): String? = ifEmpty { null }
 
 const val HEX_PREFIX = "0x"
 
+const val EURO_SIGN = '€'
+
 fun String.removeHexPrefix() = this.removePrefix(HEX_PREFIX)
--- src/main/java/jp/co/soramitsu/androidfoundation/format/TextValue.kt
+++ src/main/java/jp/co/soramitsu/androidfoundation/format/TextValue.kt
@@ -2,6 +2,7 @@ package jp.co.soramitsu.androidfoundation.format
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 
 sealed interface TextValue {
@@ -10,17 +11,44 @@ sealed interface TextValue {
     data class StringRes(val id: Int) : TextValue
 
     data class StringResWithArgs(val id: Int, val payload: Array<Any>) : TextValue {
-
         override fun equals(other: Any?): Boolean {
-            if (other !is StringResWithArgs) {
-                return false
-            }
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
 
-            return payload.contentEquals(other.payload)
+            other as StringResWithArgs
+
+            if (id != other.id) return false
+            if (!payload.contentEquals(other.payload)) return false
+
+            return true
         }
 
         override fun hashCode(): Int {
-            return 137 * id.hashCode() + payload.contentHashCode()
+            var result = id
+            result = 31 * result + payload.contentHashCode()
+            return result
+        }
+    }
+
+    data class StringPluralWithArgs(val id: Int, val amount: Int, val payload: Array<Any>) : TextValue {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as StringPluralWithArgs
+
+            if (id != other.id) return false
+            if (amount != other.amount) return false
+            if (!payload.contentEquals(other.payload)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = id
+            result = 31 * result + amount
+            result = 31 * result + payload.contentHashCode()
+            return result
         }
     }
 }
@@ -30,10 +58,16 @@ fun TextValue.retrieveString(): String = when (this) {
     is TextValue.StringRes -> stringResource(id = id)
     is TextValue.StringResWithArgs -> stringResource(id = id, formatArgs = payload)
     is TextValue.SimpleText -> text
+    is TextValue.StringPluralWithArgs -> pluralStringResource(
+        id = id,
+        count = amount,
+        formatArgs = payload,
+    )
 }
 
 fun TextValue.retrieveString(context: Context): String = when (this) {
     is TextValue.StringRes -> context.getString(id)
-    is TextValue.StringResWithArgs -> context.getString(id, payload)
+    is TextValue.StringResWithArgs -> context.getString(id, *payload)
     is TextValue.SimpleText -> text
+    is TextValue.StringPluralWithArgs -> context.resources.getQuantityString(id, amount, *payload)
 }
